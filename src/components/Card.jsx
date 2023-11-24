1a392a13ffa5447dd8f18fe2a60e276cff77c88d
Michał Kozak <live.m851@gmail.com>
2023-11-24 19:32:02 +0100

    Fix toggle button functionality in Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,15 +4,18 @@ import { Title } from "./CardCompontents/Title"
 import { Description } from "./CardCompontents/Description"
 import { useState } from "react"
 export const Card = ({title,description}) => {
-    const [isDelete,setDelete] = useState(false)
+    const [isExist,setDelete] = useState(true)
     const handleButton = () => {
-        
+        setDelete(!isExist)
     }
     return (
+        <>
+        {isExist ? (
         <div className="w-full h-48 bg-white rounded-3xl m-3 flex flex-col items-center">
             <Title>{title}</Title>
             <Description>{description}</Description>
             <Button onSelected={handleButton}>Usuń</Button>
-        </div> 
+        </div> ): undefined}
+        </>   
     )
 }
