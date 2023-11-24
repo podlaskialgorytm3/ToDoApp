78d41340be7e70cd1289abfda2429a0af3ba50f4
Michał Kozak <live.m851@gmail.com>
2023-11-24 19:20:12 +0100

    Add Title and Description components to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,7 @@
 
 import { Button } from "./CardCompontents/Button"
+import { Title } from "./CardCompontents/Title"
+import { Description } from "./CardCompontents/Description"
 import { useState } from "react"
 export const Card = ({title,description}) => {
     const [isDelete,setDelete] = useState(false)
@@ -8,8 +10,8 @@ export const Card = ({title,description}) => {
     }
     return (
         <div className="w-full h-48 bg-white rounded-3xl m-3 flex flex-col items-center">
-            
-            
+            <Title>{title}</Title>
+            <Description>{description}</Description>
             <Button onSelected={handleButton}>Usuń</Button>
         </div> 
     )
