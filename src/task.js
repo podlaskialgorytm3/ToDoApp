4825412f7eab03cf5cab133189553e3de49c4741
Michał Kozak <live.m851@gmail.com>
2023-11-24 17:53:06 +0100

    Add TASK array with different tasks

---
+++ src/task.js
@@ -0,0 +1,43 @@
+// Obiekt zawierający różne zadania
+export const TASK = [
+    {
+      title: "Zadanie 1",
+      description: "Stworzyć prostą stronę internetową z użyciem HTML i CSS."
+    },
+    {
+      title: "Zadanie 2",
+      description: "Napisać funkcję w JavaScript, która znajduje sumę liczb parzystych w danym zakresie."
+    },
+    {
+      title: "Zadanie 3",
+      description: "Zaprojektować prostą aplikację mobilną z użyciem frameworka React Native."
+    },
+    {
+      title: "Zadanie 4",
+      description: "Utworzyć bazę danych MySQL i napisać zapytanie SQL, które pobiera dane z dwóch tabel i łączy je."
+    },
+    {
+      title: "Zadanie 5",
+      description: "Napisać skrypt w Pythonie do automatycznego pobierania danych z API i zapisywania ich w pliku JSON."
+    },
+    {
+      title: "Zadanie 6",
+      description: "Stworzyć prostą grę w języku Java, na przykład grę w kółko-krzyżyk."
+    },
+    {
+      title: "Zadanie 7",
+      description: "Napisać testy jednostkowe dla istniejącego kodu w języku JavaScript, używając frameworka Jest."
+    },
+    {
+      title: "Zadanie 8",
+      description: "Zaimplementować funkcję sortującą algorytmem quicksort."
+    },
+    {
+      title: "Zadanie 9",
+      description: "Utworzyć prostą stronę internetową z użyciem frameworka Angular."
+    },
+    {
+      title: "Zadanie 10",
+      description: "Napisać skrypt w Ruby do przetwarzania pliku tekstowego i wypisania wyników na konsoli."
+    }
+  ];
