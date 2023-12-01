85ddbcfe07e49fcd3b663fb43bf7531174d47cd7
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-02 00:42:34 +0100

    Add input functionality to add new tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import './App.css'
 import './tailwind.css';
+import { useState } from 'react';
 import {ContentPage} from './components/ContentPage.jsx'
 import {TaskContent} from './components/TaskContent.jsx';
 import {Card} from './components/Card.jsx'
 import {TASK} from './task.js'
-
+import { InputAdd } from './components/InputAdd.jsx';
 
 function App() {
+  const [currentTast,setTask] = useState(TASK)
+  const handleInput = (e) => {
+    let newTask = e.target.value
+    if(e.key === 'Enter'){
+      currentTast.push({
+        title: Math.floor(Math.random() * (10000)) + 1,
+        description: newTask
+      })
+      console.log(currentTast)
+      setTask(currentTast)
+    }
+  }
   return (
       <ContentPage>
+          
           <TaskContent>
-            {TASK.map(item => 
-                <Card title={item.title} description={item.description} key={item.title} />
+          <InputAdd onKeyPress={(e) => handleInput(e)}/>
+            {currentTast.map(item => 
+                <Card description={item.description} key={item.title} />
             )}
           </TaskContent>
       </ContentPage>
---
+++ src/components/InputAdd.jsx
@@ -0,0 +1,3 @@
+export const InputAdd = ({...props}) => (
+    <input className="m-4 w-80 h-12 rounded-xl bg-fuchsia-100" {...props}/>
+)
