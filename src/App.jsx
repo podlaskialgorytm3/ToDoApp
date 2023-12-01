8f31fd8e9e686c87165e09559c9491c445d4cfab
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-02 00:48:18 +0100

    Refactor code to follow coding conventions

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,37 @@
-import './App.css'
+import './App.css';
 import './tailwind.css';
 import { useState } from 'react';
-import {ContentPage} from './components/ContentPage.jsx'
-import {TaskContent} from './components/TaskContent.jsx';
-import {Card} from './components/Card.jsx'
-import {TASK} from './task.js'
+import { ContentPage } from './components/ContentPage.jsx';
+import { TaskContent } from './components/TaskContent.jsx';
+import { Card } from './components/Card.jsx';
+import { TASK } from './task.js';
 import { InputAdd } from './components/InputAdd.jsx';
 
 function App() {
-  const [currentTast,setTask] = useState(TASK)
+  const [currentTask, setTask] = useState(TASK);
+
   const handleInput = (e) => {
-    let newTask = e.target.value
-    if(e.key === 'Enter'){
-      currentTast.push({
-        title: Math.floor(Math.random() * (10000)) + 1,
-        description: newTask
-      })
-      console.log(currentTast)
-      setTask(currentTast)
+    if (e.key === 'Enter') {
+      const newTask = {
+        title: Math.floor(Math.random() * 10000) + 1,
+        description: e.target.value,
+      };
+
+      setTask((prevTasks) => [...prevTasks, newTask]);
+      e.target.value = '';
     }
-  }
+  };
+
   return (
-      <ContentPage>
-          
-          <TaskContent>
-          <InputAdd onKeyPress={(e) => handleInput(e)}/>
-            {currentTast.map(item => 
-                <Card description={item.description} key={item.title} />
-            )}
-          </TaskContent>
-      </ContentPage>
-  )
+    <ContentPage>
+      <TaskContent>
+        <InputAdd onKeyPress={(e) => handleInput(e)} />
+        {currentTask.map((item) => (
+          <Card description={item.description} key={item.title} />
+        ))}
+      </TaskContent>
+    </ContentPage>
+  );
 }
 
-export default App
+export default App;
