1ea200aa76e3226fbb8bef9b9c2118c82ad18261
podlaskialgorytm3 <live.m851@gmail.com>
2023-12-03 12:55:23 +0100

    Remove unused tasks

--- src/task.js
+++ src/task.js
@@ -11,33 +11,5 @@ export const TASK = [
     {
       title: "Zadanie 3",
       description: "Zaprojektować prostą aplikację mobilną z użyciem frameworka React Native."
-    },
-    {
-      title: "Zadanie 4",
-      description: "Utworzyć bazę danych MySQL i napisać zapytanie SQL, które pobiera dane z dwóch tabel i łączy je."
-    },
-    {
-      title: "Zadanie 5",
-      description: "Napisać skrypt w Pythonie do automatycznego pobierania danych z API i zapisywania ich w pliku JSON."
-    },
-    {
-      title: "Zadanie 6",
-      description: "Stworzyć prostą grę w języku Java, na przykład grę w kółko-krzyżyk."
-    },
-    {
-      title: "Zadanie 7",
-      description: "Napisać testy jednostkowe dla istniejącego kodu w języku JavaScript, używając frameworka Jest."
-    },
-    {
-      title: "Zadanie 8",
-      description: "Zaimplementować funkcję sortującą algorytmem quicksort."
-    },
-    {
-      title: "Zadanie 9",
-      description: "Utworzyć prostą stronę internetową z użyciem frameworka Angular."
-    },
-    {
-      title: "Zadanie 10",
-      description: "Napisać skrypt w Ruby do przetwarzania pliku tekstowego i wypisania wyników na konsoli."
     }
   ];
